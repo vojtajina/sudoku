8376462d0cc28a8dffa08e21049e15e60a42475b
Vojta Jina <vojta.jina@gmail.com>
2010-08-22 18:32:48 +0100

    Initial commit

---
+++ src/org/vojtajina/sudoku/Matrix.java
@@ -0,0 +1,10 @@
+package org.vojtajina.sudoku;
+
+/**
+ * Represents whole sudoku matrix
+ * 
+ * @author Vojta Jina <vojta.jina@gmail.com>
+ */
+public class Matrix {
+
+}
---
+++ src/org/vojtajina/sudoku/Start.java
@@ -0,0 +1,14 @@
+package org.vojtajina.sudoku;
+
+public class Start {
+
+	/**
+	 * Start point of the application
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("Sudoku application...");
+
+	}
+
+}
