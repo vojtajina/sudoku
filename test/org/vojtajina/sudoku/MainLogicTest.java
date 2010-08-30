3072b58ac37689fae277e86aec17448ed72e00f9
Vojta Jina <vojta.jina@gmail.com>
2010-08-30 14:42:07 +0100

    Added unit test for MainLogic

---
+++ test/org/vojtajina/sudoku/MainLogicTest.java
@@ -0,0 +1,34 @@
+package org.vojtajina.sudoku;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainLogicTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public final void testCheckRow() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCheckCol() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCheckBox() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCheck() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
